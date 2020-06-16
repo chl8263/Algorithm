2f061a74d9f4610614b3fa145a80d09d5073449f
Ewan <chl8263@naver.com>
2020-06-15 17:48:45 -0700

    bruteForceSearch - Back2309

--- src/main/kotlin/bruteForceSearch/Back2309.kt
+++ src/main/kotlin/bruteForceSearch/Back2309.kt
@@ -1,8 +1,39 @@
 package bruteForceSearch
 
+import java.util.*
+
 object Back2309 {
     @JvmStatic
     fun main(args: Array<String>){
 
+//        val s = Scanner(System.`in`)
+//
+//        val given = mutableListOf<Int>()
+//
+//        for(i in 1..9){
+//            given.add(s.nextInt())
+//        }
+
+        val given = arrayOf(20,7,23,19,10,15,25,8,13)
+
+        var result = listOf<Int>()
+
+        for((i,v) in given.withIndex()){
+
+            for((j,v) in given.withIndex()){
+                if(i != j){
+                    val filter = given.filter { x -> x != given[i] && x != given[j]}
+                    val reducer = filter.fold(0){ acc, value -> acc + value }
+                    if(reducer == 100){
+                        result = filter.sorted()
+                        break
+                    }
+                }
+            }
+        }
+
+        result.forEach {
+            println(it)
+        }
     }
 }
