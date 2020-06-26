88170a6ca96004b43bfb79c5763833154b3cdb3b
ChoiWonGyun (Ewan) <chl8263@naver.com>
2020-06-26 15:28:26 -0700

    Back2839

---
+++ src/main/kotlin/Back2839.kt
@@ -0,0 +1,31 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+object Back2839 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        val st = StringTokenizer(br.readLine())
+
+        var given = st.nextToken().toInt()
+
+        var count = 0
+        while (true) {
+            if (given == 0) break
+            if (given < 0) {
+                count = -1
+                break
+            }
+            if (given % 5 == 0) {
+                given -= 5
+                count++
+            } else {
+                given -= 3
+                count++
+            }
+
+        }
+        println(count)
+    }
+}
