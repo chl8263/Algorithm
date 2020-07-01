7710aa80795f72da160a073182137c6dbaba36a6
ChoiWonGyun (Ewan) <chl8263@naver.com>
2020-07-01 16:27:03 -0700

    BOJ1978.kt

---
+++ src/main/kotlin/BOJ1011.kt
@@ -0,0 +1,12 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+object BOJ1011 {
+    @JvmStatic
+    fun main(args: Array<String>){
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        val st = StringTokenizer(br.readLine())
+
+    }
+}
