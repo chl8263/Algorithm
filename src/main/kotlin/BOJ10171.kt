6ea3a14f00bde6e12eef22954904e021457a749b
Ewan <chl8263@naver.com>
2020-07-03 15:22:46 -0700

    BOJ10171.kt

---
+++ src/main/kotlin/BOJ10171.kt
@@ -0,0 +1,14 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+object BOJ10171 {
+    @JvmStatic
+    fun main(args: Array<String>){
+
+        println("\\    /\\")
+        println(" )  ( ')")
+        println("(  /  )")
+        println(" \\(__)|")
+    }
+}
