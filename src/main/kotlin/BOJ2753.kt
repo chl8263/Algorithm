00e69646b88f73a9c93d397d1e7ff72b53bf5507
Ewan <chl8263@naver.com>
2020-07-03 15:51:03 -0700

    BOJ2753.kt

---
+++ src/main/kotlin/BOJ2753.kt
@@ -0,0 +1,34 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+object BOJ2753 {
+    @JvmStatic
+    fun main(args: Array<String>){
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        val st = StringTokenizer(br.readLine())
+
+        val one = st.nextToken().toInt()
+        val two = st.nextToken().toInt()
+        val three = st.nextToken().toInt()
+
+        val arr = arrayOf(one, two, three)
+
+        for(i in 0 until arr.size){
+            if(i == arr.size -1) break
+            if(arr[i] > arr[i+1]){
+                val t = arr[i+1]
+                arr[i+1] = arr[i]
+                arr[i] = t
+            }
+            if(i == 0){
+                if(arr[i] > arr[i+2]){
+                    val t = arr[i+2]
+                    arr[i+2] = arr[i]
+                    arr[i] = t
+                }
+            }
+        }
+        arr.forEach { print("$it ") }
+    }
+}
