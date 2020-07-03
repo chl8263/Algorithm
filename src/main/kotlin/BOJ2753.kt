7893a1b57b402b20062173c72233f7e4a1e39cda
Ewan <chl8263@naver.com>
2020-07-03 15:59:51 -0700

    BOJ2753.kt

--- src/main/kotlin/BOJ2753.kt
+++ src/main/kotlin/BOJ2753.kt
@@ -8,27 +8,9 @@ object BOJ2753 {
         val br = BufferedReader(InputStreamReader(System.`in`))
         val st = StringTokenizer(br.readLine())
 
-        val one = st.nextToken().toInt()
-        val two = st.nextToken().toInt()
-        val three = st.nextToken().toInt()
+        val given = st.nextToken().toInt()
 
-        val arr = arrayOf(one, two, three)
-
-        for(i in 0 until arr.size){
-            if(i == arr.size -1) break
-            if(arr[i] > arr[i+1]){
-                val t = arr[i+1]
-                arr[i+1] = arr[i]
-                arr[i] = t
-            }
-            if(i == 0){
-                if(arr[i] > arr[i+2]){
-                    val t = arr[i+2]
-                    arr[i+2] = arr[i]
-                    arr[i] = t
-                }
-            }
-        }
-        arr.forEach { print("$it ") }
+        if(given % 4 == 0 && given % 100 != 0 || given % 400 == 0) print(1)
+        else print(0)
     }
 }
