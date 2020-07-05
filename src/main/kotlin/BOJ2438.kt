c49c674adbe4c4462e92277bb6e63b95e5e1ab51
Ewan <chl8263@naver.com>
2020-07-05 11:22:12 -0700

    BOJ2438.kt

--- src/main/kotlin/BOJ2438.kt
+++ src/main/kotlin/BOJ2438.kt
@@ -1,18 +1,23 @@
-package sample
-
 import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.util.*
 
-object App {
+object BOJ2438 {
     @JvmStatic
     fun main(args: Array<String>){
         val br = BufferedReader(InputStreamReader(System.`in`))
         val bw = BufferedWriter(OutputStreamWriter(System.`out`))
 
         val st = StringTokenizer(br.readLine())
+        val num = st.nextToken().toInt()
+
+        for(i in 1..num){
+            for(j in 1..i)
+                bw.write("*")
+            bw.write("\n")
+        }
 
         bw.flush()
         bw.close()
