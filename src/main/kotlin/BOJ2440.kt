1c79c8f29ab03fb68e60911aa140a32135e2c868
Ewan <chl8263@naver.com>
2020-07-05 11:27:07 -0700

    BOJ2440.kt

---
+++ src/main/kotlin/BOJ2440.kt
@@ -0,0 +1,25 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+object BOJ2440 {
+    @JvmStatic
+    fun main(args: Array<String>){
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        val bw = BufferedWriter(OutputStreamWriter(System.`out`))
+
+        val st = StringTokenizer(br.readLine())
+        val num = st.nextToken().toInt()
+
+        for(i in num downTo 1){
+            for(j in 1..i)
+                bw.write("*")
+            bw.write("\n")
+        }
+
+        bw.flush()
+        bw.close()
+    }
+}
