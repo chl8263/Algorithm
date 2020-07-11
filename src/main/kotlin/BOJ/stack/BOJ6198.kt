0289b31441be4cb20cdca1e842db872ee8c63a7b
ChoiWonGyun (Ewan) <chl8263@naver.com>
2020-07-10 17:15:40 -0700

    BOJ6198.kt

---
+++ src/main/kotlin/BOJ/stack/BOJ6198.kt
@@ -0,0 +1,41 @@
+package BOJ.stack
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+object BOJ6198 {
+    @JvmStatic
+    fun main(args: Array<String>){
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        val bw = BufferedWriter(OutputStreamWriter(System.`out`))
+        val st = StringTokenizer(br.readLine())
+
+        val num = st.nextToken().toInt()
+
+        val arr = Array(num){0}
+        val stack = Stack<Int>()
+        var result = 0
+
+        for(i in 0 until num){
+            val st2 = StringTokenizer(br.readLine())
+            val building = st2.nextToken().toInt()
+            arr[i] = building
+        }
+
+        for(i in 0 until num){
+            while(!stack.isEmpty() && stack.peek() <= arr[i]){
+                stack.pop()
+            }
+            stack.push(arr[i])
+            result += stack.size-1
+        }
+
+        bw.write(result.toString())
+
+        bw.flush()
+        bw.close()
+    }
+}
