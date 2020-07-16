0bea0224d0398abcfa337abb3f2fd1c6ab83b67e
ChoiWonGyun (Ewan) <chl8263@naver.com>
2020-07-16 16:59:01 -0700

    BOJ2504.kt_

--- src/main/kotlin/BOJ/stack/BOJ2504.kt
+++ src/main/kotlin/BOJ/stack/BOJ2504.kt
@@ -15,27 +15,82 @@ object BOJ2504 {
         val st = StringTokenizer(br.readLine())
         val given = st.nextToken()
 
-        val s = Stack<Char>()
-        val result = 0
-        val temp = ArrayList<Int>()
-        var index = 0
-        for(i in given.indices){
-            if(given[i] == '(' || given[i] == '['){
+        val s = Stack<Any>()
+        var flag = true
+        var result = 0
+        var cnt1 = 0
+        var cnt2 = 0
+        loop@for(i in given.indices){
+            if(given[i] == '('){
+                if(++cnt1 < 0 ) {
+                    flag = false
+                    break@loop
+                }
                 s.push(given[i])
-                if(temp.isNotEmpty()){
-
+            }else if(given[i] == '['){
+                if(++cnt2 < 0 ) {
+                    flag = false
+                    break@loop
                 }
+                s.push(given[i])
             }else if(given[i] == ')'){
-                s.pop()
-                if(s.isEmpty()){
-                    // calculate
-                }else {
-                    temp.add(2)
-                    index++
+                if(--cnt1 < 0 ) {
+                    flag = false
+                    break@loop
                 }
-            }else if(given[i] == ']'){
+                if(s.isEmpty()) {
+                    flag = false
+                    break@loop
+                }
+                var temp = 0
+                while(s.peek() != '('){
+                    val peek = s.pop()
+                    if(peek is Int)
+                        temp += peek
+                    else {
+                        flag = false
+                        break@loop
+                    }
+                }
+                s.pop()
+                if(temp == 0) temp = 1
+                s.push(temp * 2)
 
+            }else if(given[i] == ']'){
+                if(--cnt2 < 0 ) {
+                    flag = false
+                    break@loop
+                }
+                if(s.isEmpty()) {
+                    flag = false
+                    break@loop
+                }
+                var temp = 0
+                while(s.peek() != '['){
+                    val peek = s.pop()
+                    if(peek is Int)
+                        temp += peek
+                    else {
+                        flag = false
+                        break@loop
+                    }
+                }
+                s.pop()
+                if(temp == 0) temp = 1
+                s.push(temp * 3)
+            }else {
+                flag = false
+                break@loop
             }
+        }
+
+        if(!flag) bw.write(0.toString())
+        else {
+
+            while (s.isNotEmpty()) {
+                result += s.pop() as Int
+            }
+            bw.write(result.toString())
         }
 
         bw.flush()
