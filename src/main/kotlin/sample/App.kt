8a4ff4835e01db61948fb85c8d8d686b7688b5ed
ChoiWonGyun (Ewan) <chl8263@naver.com>
2020-07-23 11:05:08 -0700

    BOJ1697_.kt

--- src/main/kotlin/sample/App.kt
+++ src/main/kotlin/sample/App.kt
@@ -13,24 +13,6 @@ object App {
         val bw = BufferedWriter(OutputStreamWriter(System.`out`))
         val st = StringTokenizer(br.readLine())
 
-        val R = st.nextToken().toInt()
-        val C = st.nextToken().toInt()
-        val arr = Array(R){Array(C){""}}
-
-        for(i in 0 until R){
-            for(j in 0 until C){
-                arr[i][j] = "[$i, $j]"
-            }
-            println()
-        }
-        println()
-        for(i in 0 until R){
-            for(j in 0 until C){
-                print(arr[i][j])
-            }
-            println()
-        }
-
         bw.flush()
         bw.close()
     }
