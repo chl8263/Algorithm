08f751fbe885129683e085137d6970b2429fdbb5
ChoiWonGyun (Ewan) <chl8263@naver.com>
2020-08-12 15:43:21 -0700

    BOJ13913.kt

--- src/main/kotlin/BOJ/bfs/BOJ13913.kt
+++ src/main/kotlin/BOJ/bfs/BOJ13913.kt
@@ -19,7 +19,7 @@ object BOJ13913 {
         val visit = Array(100001){Pair(-1, -1)}
         val q: Queue<Int> = LinkedList()
         q.add(N)
-        visit[N] = Pair(0, 0)
+        visit[N] = Pair(0, -1)
 
         val vx = arrayOf(2, -1 , 1)
         var result = 0
@@ -36,7 +36,7 @@ object BOJ13913 {
                     dx =  p + vx[i]
                 }
                 if(dx < 0 || dx > 100000) continue
-                if(visit[dx].first > -1 && visit[dx].first <= visit[p].first + visitState) continue
+                if(visit[dx].first > -1) continue //&& visit[dx].first <= visit[p].first + visitState) continue
                 if(dx == K) {
                     if (visit[dx].first > -1) {
                         result = if (visit[dx].first >= visit[p].first + visitState) visit[p].first + visitState
@@ -49,7 +49,9 @@ object BOJ13913 {
                     list.add(p)
                     var t = visit[p]
                     while (true){
-                        if(t.second == 0) break
+                        if(t.second == -1) {
+                            break
+                        }
                         list.add(t.second)
                         t = visit[t.second]
                     }
@@ -62,11 +64,15 @@ object BOJ13913 {
         }
 
         bw.write("$result\n")
-        val list2 = list.reversed()
-        list2.forEach {
-            bw.write("$it ")
+        if(list.size == 0) {
+            bw.write("$N")
         }
-
+        else {
+            val list2 = list.reversed()
+            list2.forEach {
+                bw.write("$it ")
+            }
+        }
         bw.flush()
         bw.close()
     }
