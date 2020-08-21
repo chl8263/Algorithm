1680786e7dfd07e79edbfb9474a232ead9e8560a
ChoiWonGyun (Ewan) <chl8263@naver.com>
2020-08-21 14:18:35 -0700

    BOJ2447.kt

--- src/main/kotlin/BOJ/recursion/BOJ2447.kt
+++ src/main/kotlin/BOJ/recursion/BOJ2447.kt
@@ -13,11 +13,34 @@ object BOJ2447 {
         val bw = BufferedWriter(OutputStreamWriter(System.`out`))
         val st = StringTokenizer(br.readLine())
         val num = st.nextToken().toInt()
+        val arr = Array(num){Array(num){' '}}
 
-        fun func1(){
+        fun func1(n: Int, y: Int, x: Int){
+            if(n == 0) {
+                arr[y][x] = '*'
+                return
+            }
 
+            val cN = n/3
+
+            func1(cN, y, x)
+            func1(cN, y, x+cN)
+            func1(cN, y, x+cN+cN)
+            func1(cN, y+cN, x)
+            func1(cN, y+cN, x+cN+cN)
+            func1(cN, y+cN+cN, x)
+            func1(cN, y+cN+cN, x+cN)
+            func1(cN, y+cN+cN, x+cN+cN)
         }
 
+        func1(num, 0, 0)
+
+        for(i in arr){
+            for(j in i){
+                bw.write("$j")
+            }
+            bw.write("\n")
+        }
 
         bw.flush()
         bw.close()
