5fb54639d5beeda386a6503b033f3b337492e09a
ChoiWonGyun (Ewan) <chl8263@naver.com>
2020-08-26 21:42:30 -0700

    BOJ1992.kt

---
+++ src/main/kotlin/BOJ/recursion/BOJ1992.kt
@@ -0,0 +1,21 @@
+package BOJ.recursion
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+object BOJ1992 {
+    @JvmStatic
+    fun main(args: Array<String>){
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        val bw = BufferedWriter(OutputStreamWriter(System.`out`))
+        val st = StringTokenizer(br.readLine())
+
+
+
+        bw.flush()
+        bw.close()
+    }
+}
