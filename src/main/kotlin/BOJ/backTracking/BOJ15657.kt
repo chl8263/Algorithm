b764d235c061220170595275324c4fbd6c26a6cb
ChoiWonGyun (Ewan) <chl8263@naver.com>
2020-09-02 13:32:15 -0700

    BOJ15657.kt

---
+++ src/main/kotlin/BOJ/backTracking/BOJ15657.kt
@@ -0,0 +1,54 @@
+package BOJ.backTracking
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+object BOJ15657 {
+    @JvmStatic
+    fun main(args: Array<String>){
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        val bw = BufferedWriter(OutputStreamWriter(System.`out`))
+        val st = StringTokenizer(br.readLine())
+
+        val N = st.nextToken().toInt()
+        val M = st.nextToken().toInt()
+
+        val st2 = StringTokenizer(br.readLine())
+
+        val inputArr = Array(N){0}
+        for(i in 0 until N){
+            inputArr[i] = st2.nextToken().toInt()
+        }
+        inputArr.sort()
+
+        val arr = Array(10){0}
+        val isUsed = Array(10){false}
+
+        fun func1(k: Int, s: Int){
+            if(k == M){
+                for(i in 0 until k){
+                    bw.write("${inputArr[arr[i]]} ")
+                }
+                bw.write("\n")
+                return
+            }
+
+            for(i in s until N){
+                if(!isUsed[i]) {
+                    //isUsed[i] = true
+                    arr[k] = i
+                    func1(k + 1, i)
+                    //isUsed[i] = false
+                }
+            }
+        }
+
+        func1(0, 0)
+
+        bw.flush()
+        bw.close()
+    }
+}
