bd140f81150a9af059500e016ff86ff21c495356
Ewan <chl8263@naver.com>
2020-09-05 20:45:39 -0700

    BOJ9663.kt

---
+++ src/main/kotlin/BOJ/backTracking/BOJ9663.kt
@@ -0,0 +1,19 @@
+package BOJ.backTracking
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+object BOJ9663 {
+    @JvmStatic
+    fun main(args: Array<String>){
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        val bw = BufferedWriter(OutputStreamWriter(System.`out`))
+        val st = StringTokenizer(br.readLine())
+
+        bw.flush()
+        bw.close()
+    }
+}
