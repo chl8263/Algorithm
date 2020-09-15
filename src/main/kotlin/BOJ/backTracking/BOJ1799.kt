726d5f0c6a59152b38016586c9fa1012255049b7
ChoiWonGyun (Ewan) <chl8263@naver.com>
2020-09-15 16:56:50 -0700

    BOJ1799.kt

--- src/main/kotlin/BOJ/backTracking/BOJ1799.kt
+++ src/main/kotlin/BOJ/backTracking/BOJ1799.kt
@@ -5,6 +5,7 @@ import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.util.*
+import kotlin.math.max
 
 object BOJ1799 {
     @JvmStatic
@@ -14,30 +15,38 @@ object BOJ1799 {
         val st = StringTokenizer(br.readLine())
 
         val num = st.nextToken().toInt()
-        var canGoNum = 0
         val arr = Array(num){Array(num){0}}
         for(i in 0 until num){
             val st2 = StringTokenizer(br.readLine())
             for(j in 0 until num){
                 val t = st2.nextToken().toInt()
                 arr[i][j] = t
-                if(t == 1) canGoNum++
             }
         }
 
-        val results = ArrayList<Int>()
+        //val results = ArrayList<Int>()
+        var result1 = 0
+        var result2 = 0
         val v1 = Array(num * 2 - 1){false}
         val v2 = Array(num * 2 - 1){false}
 
-        fun func1(y: Int, x: Int, s: Int, k: Int){
-            if(s == (num * num)){
-                results.add(k)
+        fun func1(y: Int, x: Int, s: Int, k: Int, flag: Int){
+            if((num % 2 == 0 && s == (num * num) / 2) ||
+                (num % 2 == 1 && flag == 0 && s == (num * num) / 2 + 1) ||
+                (num % 2 == 1 && flag == 1 && s == (num * num) / 2)
+            ){
+                if(flag == 0) {
+                    result1 = max(result1, k)
+                }else if(flag == 1) {
+                    result2 = max(result2, k)
+                }
+                //results.add(k)
                 return
             }
             var temp = s
 
             for(i in y until num){
-                for(j in x until num){
+                for(j in x until num step 2){
                     temp++
                     if(arr[i][j] == 0) continue
                     if(v1[i + j]) continue
@@ -48,17 +57,18 @@ object BOJ1799 {
                     var tx = 0
                     var ty = 0
 
-                    if(j == num-1){
-                        ty = y+1
+                    if (j == num - 2) {
+                        ty = y + 1
                         tx = 0
-                    }else {
+                    } else if (j == num - 1) {
+                        ty = y + 1
+                        tx = 1
+                    } else {
                         ty = y
-                        tx = x+1
+                        tx = x
                     }
 
-                    func1(ty, tx, temp, k+1)
-
-                    //func1(i, j, temp, k+1)
+                    func1(ty, tx, temp, k+1, flag)
 
                     v1[i + j] = false
                     v2[num - 1 - i + j] = false
@@ -66,9 +76,13 @@ object BOJ1799 {
             }
         }
 
-        func1(0, 0, 0, 0)
+        func1(0, 0, 0, 0, 0)
+        func1(0, 1, 0, 0, 1)
 
-        bw.write("${results.max()}")
+        bw.write("${result1 + result2}")
+        bw.write("${result1}")
+        bw.write("${result2}")
+        //bw.write("${results.max()}")
 
         bw.flush()
         bw.close()
