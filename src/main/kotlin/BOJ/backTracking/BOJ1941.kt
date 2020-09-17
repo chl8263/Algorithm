8d5b20f2b6f5aedd746807bffd8889b922a983c9
Ewan <chl8263@naver.com>
2020-09-16 21:01:23 -0700

    BOJ1941.kt

--- src/main/kotlin/BOJ/backTracking/BOJ1941.kt
+++ src/main/kotlin/BOJ/backTracking/BOJ1941.kt
@@ -1,15 +1,29 @@
+package BOJ.backTracking
+
 import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.util.*
 
-object App {
+object BOJ1941 {
     @JvmStatic
     fun main(args: Array<String>){
         val br = BufferedReader(InputStreamReader(System.`in`))
         val bw = BufferedWriter(OutputStreamWriter(System.`out`))
-        val st = StringTokenizer(br.readLine())
+
+        val arr = Array(5){Array(5){'x'}}
+        for(i in 0 until 5){
+            val st = br.readLine()
+            for(j in 0 until 5){
+                arr[i][j] = st[j]
+            }
+        }
+
+        fun fun1(s: Int){
+
+        }
+
 
         bw.flush()
         bw.close()
