3a8d702762a8b07e33ae7a42aad87d4dca04d2c3
ChoiWonGyun (Ewan) <chl8263@naver.com>
2020-09-28 13:44:48 -0700

    BOJ18808.kt

--- src/main/kotlin/BOJ/simulation/BOJ18808.kt
+++ src/main/kotlin/BOJ/simulation/BOJ18808.kt
@@ -7,68 +7,99 @@ import java.io.OutputStreamWriter
 import java.util.*
 
 object BOJ18808 {
+
+    var Y = 0
+    var X = 0
     @JvmStatic
     fun main(args: Array<String>){
         val br = BufferedReader(InputStreamReader(System.`in`))
         val bw = BufferedWriter(OutputStreamWriter(System.`out`))
-        //val st = StringTokenizer(br.readLine())
+        val st = StringTokenizer(br.readLine())
 
-//        val Y = st.nextToken().toInt()
-//        val X = st.nextToken().toInt()
-//        val K = st.nextToken().toInt()
-//
-//        val stickers = ArrayList<Array<Array<Int>>>()
-//        val arr = Array(Y){Array(X){0}}
-//
-//        for(i in 0 until K){
-//            val st2 = StringTokenizer(br.readLine())
-//            val Y2 = st2.nextToken().toInt()
-//            val X2 = st2.nextToken().toInt()
-//            val t_arr = Array(Y2){Array(X2){0}}
-//
-//            for(j in 0 until Y2){
-//                val st3 = StringTokenizer(br.readLine())
-//                for(h in 0 until X2){
-//                    t_arr[j][h] = st3.nextToken().toInt()
-//                }
-//            }
-//            stickers.add(t_arr)
-//        }
+         Y = st.nextToken().toInt()
+         X = st.nextToken().toInt()
+        val K = st.nextToken().toInt()
 
-        val st2 = StringTokenizer(br.readLine())
-        val Y2 = st2.nextToken().toInt()
-        val X2 = st2.nextToken().toInt()
-        var t_arr = Array(Y2){Array(X2){0}}
+        val stickers = ArrayList<Array<Array<Int>>>()
+        var arr = Array(Y){Array(X){0}}
 
-        for(j in 0 until Y2){
-            val st3 = StringTokenizer(br.readLine())
-            for(h in 0 until X2){
-                t_arr[j][h] = st3.nextToken().toInt()
+        for(i in 0 until K){
+            val st2 = StringTokenizer(br.readLine())
+            val Y2 = st2.nextToken().toInt()
+            val X2 = st2.nextToken().toInt()
+            val t_arr = Array(Y2){Array(X2){0}}
+
+            for(j in 0 until Y2){
+                val st3 = StringTokenizer(br.readLine())
+                for(h in 0 until X2){
+                    t_arr[j][h] = st3.nextToken().toInt()
+                }
+            }
+            stickers.add(t_arr)
+        }
+
+        for(sticker in stickers){
+            var t_sticker = sticker
+            loop@for(k in 0..3){
+                for(i in 0 until Y){
+                    for(j in 0 until X){
+                        if(checkDuplicate(arr, i, j, t_sticker)){
+                            arr = drewUpArr(arr, i, j, t_sticker)
+                            break@loop
+                        }
+                    }
+                }
+                t_sticker = rotateArr(t_sticker.size, t_sticker[0].size, t_sticker)
             }
         }
 
-        val t_arr2 = rotateArr(Y2, X2, t_arr)
-
-        for(i in t_arr2){
-            for(j in i){
-                print(j)
+        var ans = 0
+        for(tY in arr){
+            for(tX in tY){
+                if(tX == 1) ans++
             }
-            println()
         }
+
+        bw.write("$ans")
 
         bw.flush()
         bw.close()
     }
-}
 
-fun rotateArr(y:Int, x: Int, arr: Array<Array<Int>>): Array<Array<Int>> {
-    val result = Array(x){Array(y){0}}
 
-    for(i in 0 until x){
-        for(j in 0 until y){
-            result[i][j] = arr[j][y-i]
+    fun checkDuplicate(fArr: Array<Array<Int>>, fY: Int, fX: Int, compare: Array<Array<Int>>): Boolean {
+        var result = true
+
+        loop@for((i, t_y) in compare.withIndex()){
+            for((j, t_x) in t_y.withIndex()){
+                if(fY + i >= Y  || fX + j >= X || (compare[i][j] == 1 && fArr[fY + i][fX + j] == 1)) {
+                    result = false
+                    break@loop
+                }
+            }
         }
+        return result
     }
 
-    return result
+    fun drewUpArr(fArr: Array<Array<Int>>, fY: Int, fX: Int, compare: Array<Array<Int>>): Array<Array<Int>> {
+        for((i, t_y) in compare.withIndex()){
+            for((j, t_x) in t_y.withIndex()){
+                if(compare[i][j] == 1 && fArr[fY + i][fX + j] == 0) {
+                    fArr[fY + i][fX + j] = 1
+                }
+            }
+        }
+        return fArr
+    }
+
+    fun rotateArr(y: Int, x: Int, arr: Array<Array<Int>>): Array<Array<Int>> {
+        val result = Array(x){Array(y){0}}
+
+        for(i in 0 until y){
+            for(j in 0 until x){
+                result[j][y-1-i] = arr[i][j]
+            }
+        }
+        return result
+    }
 }
