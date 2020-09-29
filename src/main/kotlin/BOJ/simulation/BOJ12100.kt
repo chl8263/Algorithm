fe45372438c4c2c7f6a395443984b444052f998d
ChoiWonGyun (Ewan) <chl8263@naver.com>
2020-09-29 16:41:28 -0700

    BOJ12100.kt

---
+++ src/main/kotlin/BOJ/simulation/BOJ12100.kt
@@ -0,0 +1,21 @@
+package BOJ.simulation
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+object BOJ12100 {
+    @JvmStatic
+    fun main(args: Array<String>){
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        val bw = BufferedWriter(OutputStreamWriter(System.`out`))
+        val st = StringTokenizer(br.readLine())
+
+
+
+        bw.flush()
+        bw.close()
+    }
+}
