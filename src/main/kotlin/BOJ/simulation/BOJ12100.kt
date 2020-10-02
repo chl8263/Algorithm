14fe905e26ae94e238ef79d2c9bde40308030f0f
ChoiWonGyun (Ewan) <chl8263@naver.com>
2020-10-02 10:29:44 -0700

    BOJ12100.kt

--- src/main/kotlin/BOJ/simulation/BOJ12100.kt
+++ src/main/kotlin/BOJ/simulation/BOJ12100.kt
@@ -5,6 +5,7 @@ import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.util.*
+import kotlin.math.max
 
 object BOJ12100 {
     val br = BufferedReader(InputStreamReader(System.`in`))
@@ -24,28 +25,28 @@ object BOJ12100 {
         for(i in 0 until num){
             val st2 = StringTokenizer(br.readLine())
             for(j in 0 until num){
-                arr2[i][j] = st2.nextToken().toInt()
+                arr[i][j] = st2.nextToken().toInt()
             }
         }
 
-//        for(i in 0..1023){
-//            copyArr()
-//            var brute = i
-//            for(j in 0..4){
-//                val dir = brute % 4
-//                brute /= 4
-//                tilt(dir)
-//            }
-//        }
+        var max = 0
 
-        arr2 = tilt(1)
-
-        for(i in 0 until num){
-            for(j in 0 until num){
-                print(arr2[i][j])
+        for(i in 0..1023){
+            copyArr()
+            var brute = i
+            for(j in 0..4){
+                val dir = brute % 4
+                brute /= 4
+                arr2 = tilt(dir)
             }
-            println()
+            for(i in 0 until num){
+                for(j in 0 until num){
+                    max = max(arr2[i][j], max)
+                }
+            }
         }
+
+        bw.write("$max")
 
         bw.flush()
         bw.close()
@@ -56,31 +57,42 @@ object BOJ12100 {
         val y = arr2.size
         val x = arr2[0].size
 
-        val t_arr = Array(y){Array(x){0}}
+        var t_arr = Array(y){Array(x){0}}
         val isAccu = Array(y){Array(x){false}}
+
+        for(i in 0 until direction){
+            arr2 = rotate(arr2)
+        }
 
         for(i in 0 until y){
             var index = 0
             for(j in 0 until x){
-                if(arr2[i][j] != 0){
-                    if(j == 0){
-                        t_arr[i][index] = arr2[i][j]
-                    } else if(isAccu[i][index]){
-                        t_arr[i][++index] = arr2[i][j]
+                if(arr2[i][j] == 0) continue
+                if(t_arr[i][index] == 0){
+                    t_arr[i][index] = arr2[i][j]
+                } else if(isAccu[i][index]){
+                    t_arr[i][++index] = arr2[i][j]
+                }else if(!isAccu[i][index]){
+                    if(t_arr[i][index] == arr2[i][j]){
+                        t_arr[i][index] +=  arr2[i][j]
+                        isAccu[i][index] = true
                     }else {
-                        if(t_arr[i][index] == arr2[i][j] && !isAccu[i][index]){
-                            t_arr[i][index] +=  arr2[i][j]
-                        }else {
-                            t_arr[i][++index] +=  arr2[i][j]
-                        }
+                        t_arr[i][++index] +=  arr2[i][j]
                     }
                 }
             }
         }
+
+        if(direction != 0) {
+            for (i in 0 until 4 - direction) {
+                t_arr = rotate(t_arr)
+            }
+        }
+
         return t_arr
     }
 
-    fun rotate(arr: Array<Array<Int>>){
+    fun rotate(arr: Array<Array<Int>>): Array<Array<Int>> {
         val y = arr.size
         val x = arr[0].size
 
@@ -88,9 +100,10 @@ object BOJ12100 {
 
         for(i in 0 until y){
             for(j in 0 until x){
-                t[i][j] = arr[][]
+                t[j][y-1-i] = arr[i][j]
             }
         }
+        return t
     }
 
     fun copyArr(){
