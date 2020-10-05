bbe6bc5ae704e4137d47f004f7b7aff82ea36026
ChoiWonGyun (Ewan) <chl8263@naver.com>
2020-10-05 11:31:31 -0700

    BOJ13335.kt

--- src/main/kotlin/BOJ/simulation/BOJ13335.kt
+++ src/main/kotlin/BOJ/simulation/BOJ13335.kt
@@ -11,23 +11,49 @@ object BOJ13335 {
     val bw = BufferedWriter(OutputStreamWriter(System.`out`))
 
     @JvmStatic
-    fun main(args: Array<String>){
+    fun main(args: Array<String>) {
         val st = StringTokenizer(br.readLine())
 
         val n = st.nextToken().toInt()
         val w = st.nextToken().toInt()
         val l = st.nextToken().toInt()
 
-        val arr = Array(n){0}
+        val arr = Array(n) { 0 }
 
         val st2 = StringTokenizer(br.readLine())
-        for(i in 1..n){
-            arr[i-1] = st2.nextToken().toInt()
+        for (i in 1..n) {
+            arr[i - 1] = st2.nextToken().toInt()
         }
 
-        while(true){
+        var ans = 0
+        val q: Queue<Int> = LinkedList()
 
+        var weight = 0
+
+        for (truck in arr) {
+            loop@while (true) {
+                if(q.isEmpty()){
+                    q.add(truck)
+                    ans++
+                    weight += truck
+                    break@loop
+                }else if(q.size == w){
+                    weight -= q.poll()
+                }else {
+                    if(weight + truck > l){
+                        q.add(0)
+                        ans++
+                    }else {
+                        q.add(truck)
+                        ans++
+                        weight += truck
+                        break@loop
+                    }
+                }
+            }
         }
+
+        bw.write("${ans+w}")
 
         bw.flush()
         bw.close()
