599edb55ba598d1c8e1c8794cb70b74f6d790deb
ChoiWonGyun (Ewan) <chl8263@naver.com>
2020-10-07 16:52:31 -0700

    BOJ14891.kt

---
+++ src/main/kotlin/BOJ/simulation/BOJ14891.kt
@@ -0,0 +1,24 @@
+package BOJ.simulation
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+import kotlin.collections.ArrayList
+
+object BOJ14891 {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.`out`))
+
+    @JvmStatic
+    fun main(args: Array<String>){
+        val st = StringTokenizer(br.readLine())
+
+        val arr = Array(Deque<>){}
+        val circle_1: Deque<Int> = LinkedList()
+
+        bw.flush()
+        bw.close()
+    }
+}
