d161cbce85a359cf43a265e9901b29eb2e765052
wchoi <chl8263@naver.com>
2020-10-16 16:12:02 -0700

    BOJ14500

--- src/main/kotlin/BOJ/simulation/BOJ14500.kt
+++ src/main/kotlin/BOJ/simulation/BOJ14500.kt
@@ -23,17 +23,6 @@ object BOJ14500 {
 
         arr = Array(y){Array(x){0}}
 
-        val shape1 = Array(1){Array(4){1}}
-        val shape2 = Array(2){Array(2){1}}
-        val shape3 = Array(3){Array(2){0}}
-        shape3[0][0] = 1; shape3[1][0] = 1; shape3[2][0] = 1; shape3[2][1] = 1
-        val shape4 = Array(3){Array(2){0}}
-        shape4[0][0] = 1; shape4[1][0] = 1; shape4[1][1] = 1; shape4[2][1] = 1
-        var shape5 = Array(2){Array(3){0}}
-        shape5[0][0] = 1; shape5[0][1] = 1; shape5[0][2] = 1; shape5[1][1] = 1;
-
-        val shapeList = arrayListOf(shape1, shape2, shape3, shape4, shape5)
-
         for(i in 0 until y){
             val st2 = StringTokenizer(br.readLine())
             for(j in 0 until x){
@@ -41,36 +30,11 @@ object BOJ14500 {
             }
         }
 
-        for(s in 0..4) {
-            val currentShape = shapeList[s]
+        
 
-            for(i in 0..3){
-
-            }
-
-            for(i in 0..3){
-
-            }
-        }
 
         bw.flush()
         bw.close()
     }
 
-    fun rotate(shape: Array<Array<Int>>): Array<Array<Int>> {
-        val y = shape.size
-        val x = shape[0].size
-        val result = Array(x){Array(y){0}}
-
-        for(i in 0 until y){
-            for(j in 0 until x){
-                result[j][y-1-i] = shape[i][j]
-            }
-        }
-        return result
-    }
-
-    fun check(){
-
-    }
 }
