4d5f684d84f12ca7687e6ece358773063ea5698d
Ewan-desktop <chl8263@naver.com>
2020-11-02 23:14:23 +0900

    BOJ16234.kt

---
+++ src/main/kotlin/BOJ/simulation/BOJ16234.kt
@@ -0,0 +1,21 @@
+package BOJ.simulation
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+object BOJ16234 {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.`out`))
+
+    @JvmStatic
+    fun main(args: Array<String>){
+        val st = StringTokenizer(br.readLine())
+
+
+        bw.flush()
+        bw.close()
+    }
+}
