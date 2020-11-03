00e8954cbb5aef5296fa4e9253a1be4b0f958c8a
Ewan-desktop <chl8263@naver.com>
2020-11-03 22:23:47 +0900

    BOJ16234.kt

--- src/main/kotlin/BOJ/simulation/BOJ16234.kt
+++ src/main/kotlin/BOJ/simulation/BOJ16234.kt
@@ -14,6 +14,37 @@ object BOJ16234 {
     fun main(args: Array<String>){
         val st = StringTokenizer(br.readLine())
 
+        val num = st.nextToken().toInt()
+        val L = st.nextToken().toInt()
+        val H = st.nextToken().toInt()
+
+        val arr = Array(num){Array(num){0}}
+        val visited = Array(num){Array(num){0}}
+
+        for(i in 0 until num) {
+            val st2 = StringTokenizer(br.readLine())
+            for (j in 0 until num) {
+                arr[i][j] = st2.nextToken().toInt()
+            }
+        }
+
+        val dy = arrayOf(0, 0, 1, -1)
+        val dx = arrayOf(1, -1, 0, 0)
+
+        val q: Queue<Pair<Int, Int>> = LinkedList()
+        while (q.isNotEmpty()){
+            val p = q.poll()
+        }
+
+
+        for(i in 0 until num) {
+            for (j in 0 until num) {
+                val q: Queue<Pair<Int, Int>> = LinkedList()
+                while (q.isNotEmpty()){
+                    val p = q.poll()
+                }
+            }
+        }
 
         bw.flush()
         bw.close()
