738f7056ca5946ce7af76f8f354cb0112f408fe0
Ewan-desktop <chl8263@naver.com>
2020-11-04 18:12:18 +0900

    BOJ16234.kt

--- src/main/kotlin/BOJ/simulation/BOJ16234.kt
+++ src/main/kotlin/BOJ/simulation/BOJ16234.kt
@@ -5,13 +5,17 @@ import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.math.abs
 
 object BOJ16234 {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.`out`))
 
+    var a: Int? = null
     @JvmStatic
     fun main(args: Array<String>){
+
         val st = StringTokenizer(br.readLine())
 
         val num = st.nextToken().toInt()
@@ -19,7 +23,6 @@ object BOJ16234 {
         val H = st.nextToken().toInt()
 
         val arr = Array(num){Array(num){0}}
-        val visited = Array(num){Array(num){0}}
 
         for(i in 0 until num) {
             val st2 = StringTokenizer(br.readLine())
@@ -31,20 +34,57 @@ object BOJ16234 {
         val dy = arrayOf(0, 0, 1, -1)
         val dx = arrayOf(1, -1, 0, 0)
 
-        val q: Queue<Pair<Int, Int>> = LinkedList()
-        while (q.isNotEmpty()){
-            val p = q.poll()
-        }
+        var ans = 0
+        var flag = true
 
+        while (flag){
 
-        for(i in 0 until num) {
-            for (j in 0 until num) {
-                val q: Queue<Pair<Int, Int>> = LinkedList()
-                while (q.isNotEmpty()){
-                    val p = q.poll()
+            val isVisited = Array(num){Array(num){false}}
+            var tmpFlag = false
+
+            for(i in 0 until num) {
+                for (j in 0 until num) {
+
+                    if(isVisited[i][j]) continue
+
+                    val check = ArrayList<Pair<Int, Int>>()
+                    val q: Queue<Pair<Int, Int>> = LinkedList()
+                    q.add(Pair(i, j))
+                    check.add(Pair(i, j))
+                    isVisited[i][j] = true
+                    var total = arr[i][j]
+
+                    while (q.isNotEmpty()){
+                        val p = q.poll()
+                        for(i in 0..3){
+                            val dy = p.first + dy[i]
+                            val dx = p.second + dx[i]
+                            if(dy < 0 || dy >= num || dx < 0 || dx >= num || isVisited[dy][dx]) continue
+                            val diff = abs(arr[p.first][p.second] - arr[dy][dx])
+                            if(diff in L..H) {
+                                check.add(Pair(dy, dx))
+                                q.add(Pair(dy, dx))
+                                isVisited[dy][dx] = true
+                                total += arr[dy][dx]
+                            }
+                        }
+                    }
+
+                    if (check.size > 1) tmpFlag = true
+
+                    var caled = total / check.size
+
+                    for(ch in check){
+                        arr[ch.first][ch.second] = caled
+                    }
                 }
             }
+
+            if(tmpFlag) ans++
+            else flag = false
         }
+
+        bw.write("$ans")
 
         bw.flush()
         bw.close()
