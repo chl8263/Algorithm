1a0231773e2ed15eaffc82ae9b4698e058bec6d2
Ewan-desktop <chl8263@naver.com>
2020-11-05 23:50:17 +0900

    BOJ16235.kt

--- src/main/kotlin/BOJ/simulation/BOJ16235.kt
+++ src/main/kotlin/BOJ/simulation/BOJ16235.kt
@@ -16,6 +16,7 @@ object BOJ16235 {
 
     var tree = Array(N){Array(N){ArrayList<Int>()}}
     var meal = Array(N){Array(N){5}}
+    var deadMeal = Array(N){Array(N){0}}
     var A = Array(N){Array(N){0}}
 
     val dy = arrayOf(0, 0, 1, -1, 1, -1, 1, -1)
@@ -31,6 +32,7 @@ object BOJ16235 {
 
         tree = Array(N){Array(N){ArrayList<Int>()}}
         meal = Array(N){Array(N){5}}
+        deadMeal = Array(N){Array(N){0}}
         A = Array(N){Array(N){0}}
 
         for(i in 0 until N){
@@ -42,33 +44,36 @@ object BOJ16235 {
 
         for(i in 0 until M){
             val st3 = StringTokenizer(br.readLine())
-            val y = st3.nextToken().toInt() - 1
             val x = st3.nextToken().toInt() - 1
+            val y = st3.nextToken().toInt() - 1
             val old = st3.nextToken().toInt()
             tree[y][x].add(old)
         }
 
         for(i in 1..K){
             spring()
+            summer()
             autom()
             winter()
-
-            for(i in 0 until N){
-                for(j in 0 until N){
-                    val treeList = tree[i][j]
-                    for(treeI in 0 until treeList.size){
-                        print("${treeList[treeI]} ")
-                    }
-                    print(" || ")
-                    println()
-                }
-            }
-            println("================")
+            
+//            for(i in 0 until N){
+//                for(j in 0 until N){
+//                    val treeList = tree[i][j]
+//                    for(treeI in 0 until treeList.size){
+//                        print("${treeList[treeI]} ")
+//                    }
+//                    print(" || ")
+//                    println()
+//                }
+//            }
+//            println("================")
         }
 
         var ans = 0
-        for(i in tree){
-            ans += i.size
+        for(i in 0 until N){
+            for(j in 0 until N){
+                ans += tree[i][j].size
+            }
         }
 
         bw.write("$ans")
@@ -85,14 +90,24 @@ object BOJ16235 {
                 var t_meal = meal[i][j]
                 for(treeI in 0 until treeList.size){
                     val treeOld = treeList[treeI]
-                    t_meal -= treeOld
-                    if (t_meal < 0) {
-                        meal[i][j] += treeOld/2
-                        treeList.remove(treeI)
+                    val cal = t_meal - treeOld
+                    if (cal < 0) {
+                        deadMeal[i][j] += (treeOld/2)
+                        treeList.removeAt(treeI)
                     }else {
                         treeList[treeI]++
+                        meal[i][j] = cal
                     }
                 }
+            }
+        }
+    }
+
+    fun summer(){
+        for(i in 0 until N){
+            for(j in 0 until N){
+                meal[i][j] += deadMeal[i][j]
+                deadMeal[i][j] = 0
             }
         }
     }
@@ -101,11 +116,11 @@ object BOJ16235 {
         for(i in 0 until N) {
             for (j in 0 until N) {
                 val treeList = tree[i][j]
-                for(t in treeList){
-                    if(t % 5 == 0){
+                for(treeIndex in 0 until treeList.size){
+                    if(treeList[treeIndex] % 5 == 0){
                         for(v in 0..7){
                             val vy = i + dy[v]
-                            val vx = i + dx[v]
+                            val vx = j + dx[v]
                             if(vy < 0 || vy >= N || vx < 0 || vx >= N) continue
                             tree[vy][vx].add(1)
                         }
--- src/main/kotlin/BOJ/simulation/BOJ16236.kt
+++ src/main/kotlin/BOJ/simulation/BOJ16236.kt
@@ -12,20 +12,89 @@ object BOJ16236 {
 
     var N = 0
     var arr = Array(N){Array(N){0}}
+    var currentShark = Triple(0, 0, 0)
+
+    val dy = arrayOf(0, 0, 1, -1)
+    val dx = arrayOf(1, -1, 0, 0)
+
     @JvmStatic
     fun main(args: Array<String>){
         val st = StringTokenizer(br.readLine())
         N = st.nextToken().toInt()
         arr = Array(N){Array(N){0}}
 
-        for(i in 0 until N){
+        for(i in 0 until N) {
             val st2 = StringTokenizer(br.readLine())
-            for(j in 0 until N){
-                arr[i][j] = st2.nextToken().toInt()
+            for (j in 0 until N) {
+                val t = st2.nextToken().toInt()
+                if(t == 9){
+                    currentShark = Triple(i, j, 2)
+                }else{
+                    arr[i][j] = t
+                }
+
             }
         }
 
+        var ans = 0
+
+        var ex = 0
+
+        var flag = true
+        while (flag) {
+            val subResult = search()
+            if(subResult.first == Int.MAX_VALUE) flag = false
+            else {
+                ans += subResult.third
+
+                currentShark = currentShark.copy(first = subResult.first, second = subResult.second)
+                arr[subResult.first][subResult.second] = 0
+                if(currentShark.third == ++ex){
+                    currentShark = currentShark.copy(third = currentShark.third + 1)
+                    ex = 0
+                }
+
+            }
+        }
+
+        bw.write("$ans")
+
         bw.flush()
         bw.close()
+    }
+
+    fun search (): Triple<Int, Int, Int> {
+        var result = Triple(Int.MAX_VALUE, Int.MAX_VALUE, Int.MAX_VALUE)
+
+        val q: Queue<Pair<Int, Int>> = LinkedList()
+        val visited = Array(N){Array(N){-1}}
+        q.add(Pair(currentShark.first, currentShark.second))
+        visited[currentShark.first][currentShark.second] = 0
+        while (q.isNotEmpty()){
+            val p = q.poll()
+            for(i in 0..3){
+                val vy = p.first + dy[i]
+                val vx = p.second + dx[i]
+                if(vy < 0 || vy >= N || vx < 0 || vx >= N || visited[vy][vx] > -1 || currentShark.third < arr[vy][vx]) continue
+                if(arr[vy][vx] > 0 && currentShark.third > arr[vy][vx]){
+                    if(result.first == Int.MAX_VALUE) {
+                        result = Triple(vy, vx, visited[p.first][p.second] + 1)
+
+                    }else if(result.third > visited[p.first][p.second] + 1){
+                        result = Triple(vy, vx, visited[p.first][p.second] + 1)
+
+                    }else if(result.third == visited[p.first][p.second] + 1){
+                        if(result.first > vy)
+                            result = Triple(vy, vx, visited[p.first][p.second] + 1)
+
+                        else if(result.first == vy && result.second > vx)
+                            result = Triple(vy, vx, visited[p.first][p.second] + 1)
+                    }
+                }
+                q.add(Pair(vy, vx))
+                visited[vy][vx] = visited[p.first][p.second] + 1
+            }
+        }
+        return result
     }
 }
