f3a3c907078c4ddb08775c3c1ad52483564b0422
Ewan <chl8263@naver.com>
2020-11-05 19:46:58 +0900

    BOJ16236.kt

--- src/main/kotlin/BOJ/simulation/BOJ16236.kt
+++ src/main/kotlin/BOJ/simulation/BOJ16236.kt
@@ -1,20 +1,31 @@
+package BOJ.simulation
+
 import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.util.*
 
-object App {
+object BOJ16236 {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.`out`))
 
+    var N = 0
+    var arr = Array(N){Array(N){0}}
     @JvmStatic
     fun main(args: Array<String>){
         val st = StringTokenizer(br.readLine())
+        N = st.nextToken().toInt()
+        arr = Array(N){Array(N){0}}
+
+        for(i in 0 until N){
+            val st2 = StringTokenizer(br.readLine())
+            for(j in 0 until N){
+                arr[i][j] = st2.nextToken().toInt()
+            }
+        }
 
         bw.flush()
         bw.close()
     }
-
-
 }
