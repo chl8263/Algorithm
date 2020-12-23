fb00eb6894ab106e3398246ff8e140f07a212f15
Ewan-desktop <chl8263@naver.com>
2020-12-23 23:46:50 +0900

    BOJ1463

--- src/main/java/BOJ/dp/BOJ1463.java
+++ src/main/java/BOJ/dp/BOJ1463.java
@@ -1,6 +1,8 @@
 package BOJ.dp;
 
 import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ1463 {
@@ -12,8 +14,21 @@ public class BOJ1463 {
         st = new StringTokenizer(br.readLine());
         int num = Integer.parseInt(st.nextToken());
 
+        int [] isvisited = new int [num];
+
+        Queue<Pair> q = new LinkedList<>();
+
         bw.write(Integer.toString(num));
         bw.flush();
+    }
+
+    class Pair{
+        int y;
+        int x;
+        public Pair(int y, int x){
+            this.y = y;
+            this.x = x;
+        }
     }
 }
 
