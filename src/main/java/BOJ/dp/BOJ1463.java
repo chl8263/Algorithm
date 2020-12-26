87b66fb4c19843d46f7ae96c1f12c297825c69d8
Ewan-desktop <chl8263@naver.com>
2020-12-27 03:19:35 +0900

    BOJ1463 - BFS

--- src/main/java/BOJ/dp/BOJ1463.java
+++ src/main/java/BOJ/dp/BOJ1463.java
@@ -6,7 +6,7 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ1463 {
-    public void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -30,7 +30,7 @@ public class BOJ1463 {
             Pair p = q.poll();
 
             if(p.x == 1){
-                bw.write(p.count);
+                ans = p.count;
                 break;
             }
             for(int i = 0; i < 3; i ++){
@@ -38,9 +38,10 @@ public class BOJ1463 {
                 if(i == 2){
                     vx = dx[i] + p.x;
                 }else {
-                    int temp = dx[i] % p.x;
+                    int temp = p.x % dx[i];
                     if(temp == 0){
-                        vx = dx[i] / p.x;
+                        vx = p.x / dx[i];
+                        if(vx == 0) continue;
                     }else continue;
                 }
 
@@ -50,10 +51,11 @@ public class BOJ1463 {
             }
         }
 
+        System.out.println(ans);
         bw.flush();
     }
 
-    class Pair{
+    static class Pair{
         int x;
         int count;
         public Pair(int count, int x){
@@ -62,4 +64,65 @@ public class BOJ1463 {
         }
     }
 }
+
+// BFS solution
+//public class BOJ1463 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+//        StringTokenizer st;
+//        st = new StringTokenizer(br.readLine());
+//        int num = Integer.parseInt(st.nextToken());
+//
+//        boolean [] isvisited = new boolean[num+1];
+//
+////        int [] dy = new int []{0, 0, -1, 1};
+////        int [] dx = new int []{-1, 1, 0, 0};
+//        int [] dx = new int []{3, 2, -1};
+//
+//        Queue<Pair> q = new LinkedList<>();
+//        q.add(new Pair(0, num));
+//        isvisited[num] = true;
+//
+//        int ans = 0;
+//
+//        while(!q.isEmpty()){
+//            Pair p = q.poll();
+//
+//            if(p.x == 1){
+//                ans = p.count;
+//                break;
+//            }
+//            for(int i = 0; i < 3; i ++){
+//                int vx;
+//                if(i == 2){
+//                    vx = dx[i] + p.x;
+//                }else {
+//                    int temp = p.x % dx[i];
+//                    if(temp == 0){
+//                        vx = p.x / dx[i];
+//                        if(vx == 0) continue;
+//                    }else continue;
+//                }
+//
+//                if(vx <= 0 || vx > num || isvisited[vx]) continue;
+//                q.add(new Pair(p.count + 1, vx));
+//                isvisited[vx] = true;
+//            }
+//        }
+//
+//        System.out.println(ans);
+//        bw.flush();
+//    }
+//
+//    static class Pair{
+//        int x;
+//        int count;
+//        public Pair(int count, int x){
+//            this.count = count;
+//            this.x = x;
+//        }
+//    }
+//}
 
