8cb977d1fbecac0c7ce87097551b6af6219c6a62
Ewan-desktop <chl8263@naver.com>
2020-12-27 16:22:07 +0900

    Renewal-project

---
+++ src/Java_ex.java
@@ -0,0 +1,7 @@
+import java.io.IOException;
+
+public class Java_ex {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Java");
+    }
+}
---
+++ src/Kotlin_ex.kt
@@ -0,0 +1,15 @@
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+object App {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.`out`))
+
+    @JvmStatic
+    fun main(args: Array<String>){
+        println("Kotlin")
+    }
+}
