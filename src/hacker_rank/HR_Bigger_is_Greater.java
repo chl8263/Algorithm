7d70e084772b2116bc81bb8c82ab90c49c044d55
Ewan-desktop <chl8263@naver.com>
2021-01-09 13:32:37 +0900

    HR_Bigger_is_Greater.java

--- src/hacker_rank/HR_Bigger_is_Greater.java
+++ src/hacker_rank/HR_Bigger_is_Greater.java
@@ -22,59 +22,85 @@ public class HR_Bigger_is_Greater {
     }
 
     static String biggerIsGreater(String w) {
+//        for(int i = 0; i < w.length(); i ++){
+//            int a = w.charAt(i) - 40;
+//            System.out.print(w.charAt(i) + " " + a);
+//            System.out.println();
+//            arr = new char[w.length()];
+//            isVisited = new boolean[w.length()];
+//            dfs(w, 0);
+//            for(int j  = 0; j < arr.length; j ++) {
+//                System.out.print(arr[i]);
+//            }
+//        }
+
+        List tempArr = new ArrayList();
         for(int i = 0; i < w.length(); i ++){
-            int a = w.charAt(i) - 40;
-            System.out.print(w.charAt(i) + " " + a);
-            System.out.println();
-            arr = new int[w.length()];
-            isVisited = new boolean[w.length()];
-            dfs(w, 0, 0);
-            for(int j  = 0; j < arr.length; j ++) {
-                System.out.print(arr[i]);
-            }
+            tempArr.add(w.charAt(i));
+        }
+        Collections.sort(tempArr);
+        String sortedW = "";
+        for(int i = 0; i < tempArr.size(); i ++){
+            sortedW += tempArr.get(i);
         }
 
         //System.out.println(w+" -> ===========");
         arr = new char[w.length()];
         isVisited = new boolean[w.length()];
         strList = new ArrayList<String>();
-        dfs(w, 0);
-        Collections.sort(strList);
-        for(String str : strList){
-            if(str.compareTo(w) > 0){
-                return str;
-            }
-        }
+        flag = true;
+        index = -1;
+        ans = "no answer";
+        dfs(w, 0, sortedW);
+//        Collections.sort(strList);
+//        for(String str : strList){
+//            if(str.compareTo(w) > 0){
+//                return str;
+//            }
+//        }
 //        for(int j  = 0; j < arr.length; j ++) {
 //            System.out.print(arr[j]);
 //        }
 
 //        System.out.println();
 //        System.out.println("===========");
-        return "no answer";
+        return ans;
     }
 
     static char [] arr = new char [0];
     static List<String> strList;
     static boolean [] isVisited = new boolean [0];
-    static void dfs(String w, int count) {
+    static int index = -1;
+    static boolean flag = true;
+    static String ans = "no answer";
 
-        if(count == w.length()){
-            StringBuilder sb = new StringBuilder();
-            for(int i  = 0; i < arr.length; i ++) {
-                sb.append(arr[i]);
-                //System.out.print(arr[i]);
+    static void dfs(String w, int count, String sortedW) {
+        if(flag) {
+            if (count == w.length()) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < arr.length; i++) {
+                    sb.append(arr[i]);
+                    //System.out.print(arr[i]);
+                }
+                strList.add(sb.toString());
+                if (sb.toString().equals(w)) {
+                    index = strList.size();
+                }
+
+                if(strList.size()-1 == index){
+                    ans = strList.get(strList.size()-1);
+                    flag = false;
+                }
+                //System.out.println();
+                return;
             }
-            strList.add(sb.toString());
-            //System.out.println();
-            return;
-        }
-        for(int i = 0; i < w.length(); i++){
-            if(isVisited[i]) continue;
-            isVisited[i] = true;
-            arr[count] = w.charAt(i);
-            dfs(w, count + 1);
-            isVisited[i] = false;
+            for (int i = 0; i < w.length(); i++) {
+                if (isVisited[i]) continue;
+                isVisited[i] = true;
+                arr[count] = sortedW.charAt(i);
+                dfs(w, count + 1, sortedW);
+                isVisited[i] = false;
+            }
         }
     }
 
